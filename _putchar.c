9ba4865419d2fec4be5cae9201bd949bef9f5e61
badawiabbass <badawi.nor@gmail.com>
2023-10-15 20:41:25 +0100

    puchar been updated

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
+/** 
+ * putchar for printing integers 
+ *
+ * Return: write with number and character
+ */
 
 int _putchar(int c)
 {
-        return write(1, &c, 1);
+	return write(1, &c, 1);
 }
