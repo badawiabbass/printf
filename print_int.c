dc2f1f33ca9ef038bd4a444652c05ed0eb4b6bda
badawiabbass <badawi.nor@gmail.com>
2023-10-15 20:36:54 +0100

    the fix been add to the file

--- print_int.c
+++ print_int.c
@@ -1,40 +1,42 @@
 #include "main.h"
-/** 
- * print_int - for print integers
- * @args: the argument of integers
+
+/**
+ * print_int - to print integers
+ * @args: The argument integers
  * Return: 1
  */
-
 int print_int(va_list args)
 {
-        int count = 0;
-        int arg = va_arg(args, int);
+	int count = 0;
+	int arg = va_arg(args, int);
+	
+	if (arg == 0)
+	{
+		_putchar('-');
+		count++;
+	}
+	if (arg < 0)
+	{
+		_putchar('-');
+		arg = -arg;
+		count++;
+	}
 
-        if (arg == 0)
-        {
-                _putchar('0');
-                count++;
-        }
-        if (arg < 0)
-        {
-                _putchar('-');
-                arg = -arg;
-                count++;
-        }
-        count += print_positive_int(arg);
-
-        return (count);
+	count + = print_positive_int(arg);
+	return count;
 }
+
+/** printing a positive number */
 
 int print_positive_int(int n)
-
 {
-        int count = 0;
-
-        if (n / 10 != 0)
-        {
-                count += print_positive_int(n / 10);
-        }
-        _putchar(n % 10 + '0');
-        return (count + 1);
+	int count = 0;
+	
+	if (n / 10 != 0)
+	{
+		count + = print_positive_int (n / 10);
+	}
+	_putchar(n % 10 + '0');
+	return (count + 1);
 }
+
