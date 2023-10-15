b482a4af88e9c02a69003cdd4ed8ee56cbe17645
badawiabbass <badawi.nor@gmail.com>
2023-10-15 19:11:26 +0100

    print_int.c been added

---
+++ print_int.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+int print_int(va_list args)
+{
+        int count = 0;
+        int arg = va_arg(args, int);
+
+        if (arg == 0)
+        {
+                _putchar('0');
+                count++;
+        }
+        if (arg < 0)
+        {
+                _putchar('-');
+                arg = -arg;
+                count++;
+        }
+        count += print_positive_int(arg);
+
+        return (count);
+}
+
+int print_positive_int(int n)
+
+{
+        int count = 0;
+
+        if (n / 10 != 0)
+        {
+                count += print_positive_int(n / 10);
+        }
+        _putchar(n % 10 + '0');
+        return (count + 1);
+}
