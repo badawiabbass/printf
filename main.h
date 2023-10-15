39fd28f8022e9d1e7a3176cc0e844ac669e80c8e
osamanazar47 <osamanazar47@gmail.com>
2023-10-15 10:47:58 -0700

    header

--- main.h
+++ main.h
@@ -7,6 +7,7 @@ int print_str(va_list args);
 int print_char(va_list args);
 int print_per(va_list args);
 int _putchar(int c);
+int print_int(va_list args);
 typedef struct printf
 {
 	char specifier;
