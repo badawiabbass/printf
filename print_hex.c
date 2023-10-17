4a00c5dc62a6fcc7352bd34c144e7f58000fb188
badawiabbass <badawi.nor@gmail.com>
2023-10-17 15:00:14 +0100

    Update print_hex.c

--- print_hex.c
+++ print_hex.c
@@ -1,51 +1,57 @@
 #include "main.h"
+
 /**
  * print_int - for print integers
  * @args: the argument of integers
  * Return: 1
-*/
+ */
 int print_int(va_list args)
 {
-	int count = 0;
-	int arg = va_arg(args, int);
+int count = 0;
+int arg = va_arg(args, int);
 
-	if (arg == 0)
-	{
-		_putchar('0');
-		count++;
-		return (count);
-		else if (arg == INT_MAX)
+		if (arg == 0)
 		{
-			_putchar('-');
-			count++;
-			count += 1;
-			return (count);
+				_putchar('0');
+				count++;
 		}
-		print_positive_int(INT_MAX / 10);
-		_putchar('8');
-		count++;
-		else if (arg < 0)
-		{
-			_putchar('-');
-			count++;
-			arg = arg;
-			count += print_positive_int(arg);
-			else
+			else if (arg == INT_MIN)
 			{
-				count += print_positive_int(arg);
+				_putchar('-');
+				count++;
+				count += 1;
 			}
-			return (count);
-		}
-  /**
-   * print_positive_int - Prints a positive integer recursively.
-   * @n: The positive integer to print.
-   *
-  */
-		int print_positive_int(int n)
-		{
-			_putchar(n / 10 + '0');
+			print_positive_int(INT_MAX / 10);
+			_putchar('8');
 			count++;
-		}
-		return (count);
-	}
+				else if (arg < 0)
+				{
+					_putchar('-');
+					count++;
+					arg = arg;
+					count += print_positive_int(arg);
+				}
+					else
+					{
+						count += print_positive_int(arg);
+					}
+					return (count);
+
+					/**
+					 * print_positive_int - Prints a positive integer recursively.
+					 * @n: The positive integer to print.
+					 *
+					 * Return: The number of digits printed
+					 */
+				int print_positive_int(int n)
+				{
+					int count = 0;
+							if (n / 10 != 0)
+							{
+								count += print_positive_int(n / 10);
+							}
+							_putchar(n % 10 + '0');
+							count++;
+							return (count);
+					}
 }
